13f1d895fd7963fa1e73f5895ae651fb0071094e
Roueple <roueple@gmail.com>
2025-05-12 23:58:23 +0700

    fix

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,6 @@ export type UserRole = SupabaseDB['public']['Enums']['user_role_enum'];
 export type Novel = SupabaseDB['public']['Tables']['novels']['Row'];
 export type Chapter = SupabaseDB['public']['Tables']['chapters']['Row'];
 export type Comment = SupabaseDB['public']['Tables']['comments']['Row'];
+export interface NovelDetails extends Novel {
+  chapters: Chapter[]; // Uses the new 'Chapter' type
+}
